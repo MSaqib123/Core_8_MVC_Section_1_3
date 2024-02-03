3148f9cca1a590b131a0e3b0a7bf9bb00bd1329a
MBoota <m43577535@gmail.com>
2024-02-03 12:42:09 +0500

    Http_class_1

--- BasiceConcept/Program.cs
+++ BasiceConcept/Program.cs
@@ -1,6 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-//_+__ basic .net ___
+//================================================
+//============ Section #  HttpContext ============
+//================================================
+
+//===== 1. MapGet ======
+//map only return values
+//app.MapGet("/", () => "Hello World!");
+
+//===== 2. Run , httpContext , async + await =======
+//to write code in block ---
+//accept httpContext parameter
+app.Run(async (HttpContext context) =>
+{
+    context.Response.StatusCode = 400;
+    await context.Response.WriteAsync("Hello");
+});
+
+//===== 2. Run , httpContext , async + await =======
+
+
 app.Run();
