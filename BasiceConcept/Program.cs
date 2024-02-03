508149767b86ba24531a3db3d797eaf48358bb0e
MBoota <m43577535@gmail.com>
2024-02-03 13:37:14 +0500

    Response_header

--- BasiceConcept/Program.cs
+++ BasiceConcept/Program.cs
@@ -1,24 +1,46 @@
+using Microsoft.AspNetCore.Http;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 //================================================
 //============ Section #  HttpContext ============
 //================================================
+//Divided into 3 section
+//1. General
+//2. Response Header
+//3. Request Headers
 
-//===== 1. MapGet ======
+//===== MapGet ======
+#region MapGet
 //map only return values
 //app.MapGet("/", () => "Hello World!");
+#endregion
 
-//===== 2. Run , httpContext , async + await =======
+
+//===== 1. Generel + Status =======
+#region httpContext + status
+//httpContext + Status , async + await
 //to write code in block ---
 //accept httpContext parameter
-app.Run(async (HttpContext context) =>
+//app.Run(async (HttpContext context) =>
+//{
+//    //context.Response.StatusCode = 400;
+//    context.Response.StatusCode = 200;
+//    await context.Response.WriteAsync("Hello");
+//});
+#endregion
+
+//===== 2. Response Header =======
+#region ResponseHeader
+app.Run(async (HttpContext context) => 
 {
-    context.Response.StatusCode = 400;
-    await context.Response.WriteAsync("Hello");
+    //Header are iheaderDictunary  (keyvaluePair)
+    context.Response.Headers["MyKye_Pakistan"] = "Hum Zindaa Qoom";
+    context.Response.Headers["Content-Type"] = "text/html";
+    await context.Response.WriteAsync("<h1>Hello</h1>");
+    await context.Response.WriteAsync("<h2>Hello</h2>");
 });
-
-//===== 2. Run , httpContext , async + await =======
-
+#endregion
 
 app.Run();
